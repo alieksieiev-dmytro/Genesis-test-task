36abd3c5f2847689e28e0b36fbef8173c05a336e
alieksieiev-nure <dmytro.alieksieiev.21@gmail.com>
2022-08-11 10:29:36 +0300

    Music and SFX added

---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    [SerializeField] private List<AudioClip> audioClips;
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private bool isEnabledSFX;
+       
+    
+    private void Start()
+    {
+        StartCoroutine(LoopBackgroundMusic());
+    }
+
+    public void PlaySound(AudioClip sound)
+    {
+        if (isEnabledSFX)
+        {
+            audioSource.PlayOneShot(sound);
+        }
+    }
+
+    public void EnableMusic(bool toEnable)
+    {
+        if (toEnable)
+        {
+            StartCoroutine(LoopBackgroundMusic());
+        }
+        else
+        {
+            StopAllCoroutines();
+        }
+    }
+
+    public void EnableSFX(bool toEnable)
+    {
+        isEnabledSFX = toEnable;
+    }
+    
+    private IEnumerator LoopBackgroundMusic()
+    {
+        foreach (var clip in audioClips)
+        {
+            audioSource.clip = clip;
+            audioSource.Play();
+            yield return new WaitForSeconds(audioSource.clip.length);
+        }
+
+        StartCoroutine(LoopBackgroundMusic());
+    }
+}
